11c167a9c3f990552ee6fea38481ddf8aadd476d
Anton Fosselius <anton.fosselius@gmail.com>
2017-08-15 20:25:10 +0200

    Added a new SEA design in FreeCAD, based on small 3-12v DC motor.

--- Actuators/ParametricSEA/Mechanics/generator/generate.py
+++ Actuators/ParametricSEA/Mechanics/generator/generate.py
@@ -7,6 +7,7 @@ import sys
 from solid import *
 from solid.utils import *
 from partList import *
+from solid.partLib import *
 use_3dp_spring = False
 
 
@@ -63,11 +64,60 @@ def SEA(sled_pos=0):
 		ESC40W(),
 	)
 
+#case = difference()(
+#		up(0)(cube((60,42,14),True)),
+#		up(2.5)(cube((62,14,11),True)),
+#		up(5)(cube((52,50,5),True)),
+#		)
+
+case = difference()(
+		union()(
+			left(8)(cube((20,42,14),True)),
+			translate((-35,19.5,-2))(cube((34,3,10),True)),
+			translate((-35,-19.5,-2))(cube((34,3,10),True)),
+			),
+		up(0)(cube((62,14,10),True)),
+		right(12)(cube((56,39.5,11.5),True)),
+		#up(5)(cube((52,50,5),True)),
+		)
+
+#case = union()(
+#		case,
+#		translate((-38,19,-2))(cube((24,4,10),True)),
+#		translate((-38,-19,-2))(cube((24,4,10),True)),
+#		)
+
+pin = cylinder(r=2.05,h=100)
+pin.add_param('$fs', 1)
+case = difference()(
+		case,
+		translate([-46,-50,-2])(rot_z_to_y(pin)),
+	)
+
+base = union()(
+		down(2)(cube((52,33,10),True)),
+		#translate([0,-50,0])(rot_z_to_y(pin)),
+	)
+
+base = difference()(
+	base,
+	translate([-16,-6,-4])(cube((10,14,10),True)),
+	translate([0,-50,0])(rot_z_to_y(pin)),
+	cube([100,3*5,3],True),
+	scale(1.05)((color(Red)(rot_z_to_y(translate([5,0,1.2])(servo()))))),
+	back(0)(cube((48,30,8),True)),
+	translate([20,-10,-10])(pin),
+	translate([-20,10,-10])(pin),
+	)
 if __name__ == '__main__':
 	out_dir = sys.argv[1] if len(sys.argv) > 1 else os.curdir
 	file_out = os.path.join(out_dir, 'SEA.scad')
 	a = union()(
-		(SEA(sled_pos)),
+		(color(Transparent)(base)),
+		translate([24,40,0])(case),
+		#translate([-2,0,46])(rot_z_to_neg_x(color(Blue)(up(0)(cube((56,38,11),True))))),
+		#translate([-2,0,46])(rot_z_to_neg_x(case)),
+		#(SEA(sled_pos)),
 	)
 	bom = bill_of_materials()
 	print("%(__file__)s: SCAD file written to: \n%(file_out)s" % vars())
